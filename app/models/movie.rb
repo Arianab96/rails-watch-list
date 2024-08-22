ecf2e2f9f1d13b165dba3aab488330a6f3daadcf
Ariana Bakhshandeh <arianabakhshandeh@gmail.com>
2024-08-22 13:29:02 +0100

    validations added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ApplicationRecord
-  belongs_to 
+  has_many :bookmarks
+
+  validates :title, uniqueness: true
+  validates :overview, uniqueness: true
+  validates :title, presence: true
+  validates :overview, presence: true
 end
