f9c952685cbf51879494f71472e09e25fb5dac14
Ariana Bakhshandeh <arianabakhshandeh@gmail.com>
2024-08-23 10:43:00 +0100

    bookmarks controller edited

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,5 +1,6 @@
 class BookmarksController < ApplicationController
   before_action :set_list, only: %i[new create]
+  before_action :set_bookmark, only: :destroy
 
   def new
     @bookmark = Bookmark.new
@@ -13,6 +14,11 @@ class BookmarksController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
   end
 
   private
